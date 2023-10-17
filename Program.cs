df934a495c1b643a4d38925408816bb2f79f6ecc
Ali Salmeh <alisalmeh18@gmail.com>
2023-10-17 11:08:22 +0330

    add firstQuery

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace AliSalmeh_ProjectWeek12_LinqPractice
 {
@@ -6,7 +7,21 @@ namespace AliSalmeh_ProjectWeek12_LinqPractice
     {
         static void Main(string[] args)
         {
-            
+            var studentList = StudentDatabase.GetStudentsFromDb();
+
+            // Active students && score > 80 && NYC city
+            var firstQuery = studentList
+            .Where(s => s.IsActive
+                        && s.Score > 80
+                        && s.City == "NYC")
+            .Select(x => x.StudentName);
+
+            Console.WriteLine("First query result is: ");
+
+            foreach (var item in firstQuery)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
